6dc6791bddec8efb937ce2c2654bb7499ac6f5dd
Muharram <hafizhfakhri25@gmail.com>
2025-12-01 12:06:44 +0700

    update be untuk deploy

--- gis-backend/server.js
+++ gis-backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const Lokasi = require('./models/lokasi');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
